b4e21ebea810be684d1d8782c285a8ce6e0134a3
Damian <damian.mateos.garcia@gmail.com>
2025-03-02 12:52:23 +0100

    MVP: v1.0.1 and new system prompt

--- src/quiz_api.py
+++ src/quiz_api.py
@@ -1,13 +1,14 @@
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from quiz_generator import QuizQuestionGenerator
+import json
 
 
 class QuizQuestionAPI:
     """
-    QuizQuestionAPI version 1.0.0
+    QuizQuestionAPI version 1.0.1
     This class provides a RESTful API interface for the QuizQuestionGenerator.
     """
 
@@ -30,20 +31,27 @@ class QuizQuestionAPI:
         Parameters:
             debug (bool): Whether to run the Flask app in debug mode.
         """
-        self._app.run(debug = debug)
+        self._app.run(debug=debug)
 
     def _setup_routes(self):
         """
         Sets up the Flask routes for the RESTful API.
         """
-        @self._app.route('/generate_question', methods = ['POST'])
+        @self._app.route('/generate_question', methods=['POST'])
         def generate_question():
             data = request.json
             learning_objective = data.get('learning_objective')
             question = self._generator.generate_questions(learning_objective)
-            return jsonify({'question': question})
+            try:
+                # Attempt to parse the question to JSON format
+                question_dict = json.loads(question)
+            except json.JSONDecodeError:
+                # Handle the case where the JSON format is imperfect
+                question_str = question.replace('\n', '').replace("'", '"')
+                question_dict = json.loads(question_str)
+            return jsonify(question_dict)
 
-        @self._app.route('/current_cost', methods = ['GET'])
+        @self._app.route('/current_cost', methods=['GET'])
         def current_cost():
             cost = self._generator.calculate_current_cost()
             return jsonify({'current_cost': cost})
@@ -51,4 +59,4 @@ class QuizQuestionAPI:
 if __name__ == '__main__':
     generator = QuizQuestionGenerator()
     api = QuizQuestionAPI(generator)
-    api.run(debug = True)
+    api.run(debug=True)
